0acb8ae0443b8a716bc64a302b501716a29d3e46
Le Ha Quang <winlp4ever@gmail.com>
2019-12-30 23:06:08 +0100

    rewrite component using react hooks

--- src/user-auth/user-auth.js
+++ src/user-auth/user-auth.js
@@ -1,58 +1,46 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import './_user-auth.scss';
 
-export default class Auth extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            name: '',
-            email: '',
-            pass: ''
-        }
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handlePass = this.handlePass.bind(this);
-    }
+const Auth = props => {
+    const [name, setName] = useState('');
+    const [email,setEmail] = useState('');
+    const [pass, setPass] = useState('');
 
-    async handleSubmit() {
-        if (this.state.name == 'AII') {
+    const handleSubmit = async () => {
+        if (name == 'AII') {
             let response = await fetch('/admin-verify', {
                 method: 'POST', 
                 headers: { 'Content-Type': 'application/json' },
-                body: JSON.stringify({pass: this.state.pass})
+                body: JSON.stringify({pass: pass})
             });
             let data = await response.json();
             if (data.answer != 'y') return;
-            this.setState({name: 'AII'});
+            setName('AII');
         }
-        this.props.updateUser({name: this.state.name, email: this.state.email});
+        props.updateUser({name: name, email: email});
     }
 
-    handleChange(e) {
-        this.setState({
-            name: e.target.value
-        })
+    const handleChange = e => {
+        setName(e.target.value);
     }
 
-    handlePass(e) {
-        this.setState({
-            pass: e.target.value
-        })
+    const handlePass = (e) => {
+        setPass(e.target.value)
     }
 
-    render() {
-        let input_adminpass = '';
-        if (this.state.name == 'AII') input_adminpass = <input onChange={this.handlePass} />
-        return (
-            <div className='auth' onKeyPress={e => {
-                let keycode = e.keyCode || e.which;
-                if (keycode == 13) this.handleSubmit();
-            }}>
-                <span>How do you want to be called?</span>
-                <input onChange={this.handleChange} placeholder='Enter your name'/>
-                {input_adminpass}
-                <button onClick={this.handleSubmit}>Let's go!</button>
-            </div>
-        )
-    }
+    let input_adminpass = '';
+    if (name == 'AII') input_adminpass = <input onChange={handlePass} />
+    return (
+        <div className='auth' onKeyPress={e => {
+            let keycode = e.keyCode || e.which;
+            if (keycode == 13) handleSubmit();
+        }}>
+            <span>How do you want to be called?</span>
+            <input onChange={handleChange} placeholder='Enter your name'/>
+            {input_adminpass}
+            <button onClick={handleSubmit}>Let's go!</button>
+        </div>
+    )
 }
+
+export default Auth;
