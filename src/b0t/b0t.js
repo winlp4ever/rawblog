f0eec9f8e602a3f09e356752336b2e587f22c809
Le Ha Quang <winlp4ever@gmail.com>
2020-01-09 22:41:59 +0100

    fix chatbot - demo almost done

--- src/b0t/b0t.js
+++ src/b0t/b0t.js
@@ -4,45 +4,32 @@ import { userContext } from '../user-context/user-context';
 import $ from 'jquery';
 import Img from '../../imgs/discuss.svg';
 
-const Disclaimer = () => {
-    return (
-        <div className='disclaimer'>
-            <b>ATTENTION!</b> This chatbot is just a small demo!
-        </div>
-    )
-}
-
 const B0t = props => {
     const [chats, setChats] = useState([]);
     const [newchat, setNewchat] = useState('');
-    const [hide, setHide] = useState(false)
+    const [hide, setHide] = useState(false);
+    const [dests, setDests] = useState(['bot']);
+    const [currDest, setCurrDest] = useState(0);
     
     const userdata = useContext(userContext);
 
-    console.log(userdata);
-
     useEffect(() => {
         let copy = chats.slice();
-        copy.push({user: 'b0t', msg: 'Knock Knock Neo...'});
+        copy.push({sender: 'bot', dest: userdata.user.name, msg: 'Knock Knock Neo...'});
         setChats(copy);
         props.socket.on('bot-msg', msg => {
             let copy = chats.slice();
-            copy.push({user: 'b0t', msg: msg});
-            setChats(copy)
+            copy.push({sender: 'bot', dest: userdata.user.name, msg: msg});
+            setChats(copy);
         })
     }, [])
 
-    const hideUnhide = (e) => {
-        if (hide) {
-            $(e.currentTarget).parent().parent().children('.chat-section').css({'display':''});
-        } else {
-            $(e.currentTarget).parent().parent().children('.chat-section').css({'display': 'none'});
-        }
-        setHide(!hide);
-    }
-
     const onChange = (e) => {
         setNewchat(e.target.value);
+    }
+
+    const chooseDest = (id) => {
+        setCurrDest(id);
     }
 
     const submit = async (e) => {
@@ -51,38 +38,58 @@ const B0t = props => {
         
         e.preventDefault(); // prevents page reloading
         if (!newchat) return;
-        props.socket.emit('user-msg', newchat);
         let copy = chats.slice();
-        copy.push({user: userdata.user.name, msg: newchat});
+        copy.push({sender: userdata.user.name, dest: dests[currDest], msg: newchat});
+        
+        if (dests[currDest] == 'bot') {
+            if (newchat == 'what is bert2?') {
+                copy.push({sender: 'bot', dest: userdata.user.name, msg: "I'm not qualified to answer this."});
+                copy.push({sender: 'bot', dest: userdata.user.name, msg: "I will deliver this msg to professor Alpha!"});
+                if (dests.indexOf('Prof. Alpha') < 0) {
+                    copy.push({sender: 'Prof. Alpha', dest: userdata.user.name, msg: "Hi, please ask me..."});
+                    let cp_list = dests.slice();
+                    cp_list.push('Prof. Alpha');
+                    setDests(cp_list);
+                }
+                
+            } else {
+                props.socket.emit('user-msg', newchat);
+            }
+        }
         setChats(copy);
-        setNewchat('')
+        setNewchat('');
+        
         $(e.currentTarget).val('');     
     }
 
     return (
         <div className='b0t'>
-            <Disclaimer />
-            <div className='icon'>
-                <img onClick={hideUnhide} src={Img} />
+            <div className='chat-list'>
+                {dests.map((d, id) => <span key={id} onClick={_ => chooseDest(id)}>{d}</span>)}
             </div>
             <div className='chat-section'>
-                {chats.map((c, id) => {
-                    let b0ticon = '';
-                    let cl = 'msg';
-                    if (c.user == 'b0t') {
-                        b0ticon = <div><i className="fas fa-robot"></i></div>;
-                        cl += ' bot';
-                    }
-                    return (
-                        <div key={id}>
-                            <div className={cl}>
-                                <span>{c.msg}</span>
-                                <span className='user'>:{c.user}</span>
+                <div className='oldchats'>
+                    {chats.map((c, id) => {
+                        if (c.sender != dests[currDest] && c.dest != dests[currDest]) return;
+                        let b0ticon = '';
+                        let cl = 'msg';
+                        if (c.sender == 'bot') {
+                            b0ticon = <div><i className="fas fa-robot"></i></div>;
+                            cl += ' bot';
+                        } else if (c.sender != userdata.user.name) {
+                            cl += ' others';
+                        }
+                        return (
+                            <div key={id}>
+                                <div className={cl}>
+                                    <span>{c.msg}</span>
+                                    <span className='user'>{c.sender}:</span>
+                                </div>
+                                {b0ticon}
                             </div>
-                            {b0ticon}
-                        </div>
-                    )
-                })}
+                        )
+                    })}
+                </div>
                 <div className='newchat'>
                     <textarea 
                         rows={1}
@@ -92,6 +99,7 @@ const B0t = props => {
                     />
                 </div>
             </div>
+            
             
         </div>
     )
