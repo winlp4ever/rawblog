489e71374f11d2113810740891b368bb24294d7d
Le Ha Quang <winlp4ever@gmail.com>
2020-01-17 12:21:59 +0100

    use sent2vec to output which question is mot coherent with the one on typing

--- nlp/test.py
+++ nlp/test.py
@@ -1,5 +1,8 @@
-def foo():
-    print(a)
-
-a = 5
-foo()
+import sent2vec
+from scipy.spatial.distance import cosine
+model = sent2vec.Sent2vecModel()
+model.load_model('/home/redlcamille/workspace/sent2vec/torontobooks_unigrams.bin')
+emb = model.embed_sentence("what is neural network?") 
+e = model.embed_sentence("how does neural network work?")
+embs = model.embed_sentences(["first sentence .", "another sentence"])
+print(cosine(emb, e))
