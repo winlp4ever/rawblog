30aa7c53c6b8c37081a9db273505de33b3b33c49
Le Ha Quang <winlp4ever@gmail.com>
2020-02-26 23:55:06 +0100

    allow rendering math in react

--- src/markdown-render/markdown-render.js
+++ src/markdown-render/markdown-render.js
@@ -2,23 +2,31 @@ import React, { Component } from "react";
 import ReactMarkdown from "react-markdown";
 import CodeBlock from '../syntax-highlight/syntax-highlight';
 import './_markdown-render.scss';
-
+import RemarkMathPlugin from 'remark-math';
+import 'katex/dist/katex.min.css';
+import { InlineMath, BlockMath } from 'react-katex';
 
 const MdRender = props => {
     const newProps = {
         ...props,
         escapeHtml: false, // enable html rendering
+        plugins: [
+            RemarkMathPlugin,
+        ],
         renderers: {
             ...props.renderers,
             code: CodeBlock,
-            
+            math: (props) => 
+                <BlockMath math={props.value} />,
+            inlineMath: (props) =>
+                <InlineMath math={props.value} />
         }
     };
     
     return (
 
         <div className='markdown-render'>
-                <ReactMarkdown {...newProps}/>
+            <ReactMarkdown {...newProps}/>
         </div>
         
     );
