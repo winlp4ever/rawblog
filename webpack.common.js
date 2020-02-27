00ed3b571ba01861ed50b8b12b8fec2ae5b53887
Le Ha Quang <winlp4ever@gmail.com>
2020-02-27 11:23:20 +0100

    small changes

--- webpack.common.js
+++ webpack.common.js
@@ -48,7 +48,7 @@ module.exports = {
                 ],
             },
             {
-                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+                test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg|ico)(\?[a-z0-9=.]+)?$/,
                 loader: 'url-loader?limit=100000' 
             },
             {
